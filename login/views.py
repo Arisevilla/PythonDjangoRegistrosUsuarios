5b5bc9d9c8e9e702a7180e44f013ea6148b59534
Arisevilla <arisevilla93@gmail.com>
2023-11-30 12:07:55 -0300

    corregido error de rut

--- login/views.py
+++ login/views.py
@@ -70,25 +70,34 @@ def guardar(request):
 
     def validar_rut(rut):
 
-        rut=rut.replace(".","").replace("-","").lower()
-        if not rut[:-1].isdigit() or not rut[-1] in ('0-9kK'):
+        rut=rut.replace("-","").replace(".","").upper()
+        print(f"Rut sin formato: {rut}")
+
+        if not rut[:-1].isdigit() or not rut[-1] in ('0-1-2-3-4-5-6-7-8-9K'):
+            print("Error: No es un digito o el digito verificador no es 'K' ")
             return False
+        
+        
         reversed_digits= map(int,reversed(rut[:-1]))
         factors=[2,3,4,5,6,7,2,3,4,5,6,7]
         s=sum(d*f for d, f in zip(reversed_digits,factors))
         expected_digit=(11-s % 11)% 11
-        expected_digit='k' if expected_digit == 10 else str(expected_digit)
-        return expected_digit== rut[-1].lower()
+        expected_digit='K' if expected_digit == 10 else str(expected_digit)
+
+        print(f"Valor esperado del digito verificador: {expected_digit}")
+        return expected_digit == rut[-1].lower()
     
-    if not validar_rut(rut):
+    rut_valido= validar_rut(rut)
+    
+    if rut_valido:
+            r = Formulario(cliente=cliente,rut=rut,direccion=direccion,fono=fono,descripcion=descripcion)
+            r.save()
+            messages.success(request, 'Producto agregado')
+            return redirect('listado')
+    else:
             messages.error(request,'El rut es inválido')
             return redirect('home')
-
-    else:
-        r = Formulario(cliente=cliente,rut=rut,direccion=direccion,fono=fono,descripcion=descripcion)
-        r.save()
-        messages.success(request, 'Producto agregado')
-        return redirect('listado')
+        
 
 def detalle(request,id):
     registro= Formulario.objects.get(pk=id)
