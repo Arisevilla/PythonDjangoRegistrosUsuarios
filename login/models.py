b0a4b9a7dde3ce6f20ffe31081e4dfa27459419b
Arisevilla <arisevilla93@gmail.com>
2023-11-30 10:10:14 -0300

    validar rut listo

--- login/models.py
+++ login/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
 
 # Create your models here.
 class Formulario(models.Model):
     cliente = models.CharField(max_length=30)
     fecha = models.DateTimeField(auto_now_add=True)
-    rut = models.CharField(max_length=12)
+    rut = models.CharField(max_length=12,unique=True)
     direccion = models.CharField(max_length=100)
-    fono = models.FloatField()
+    fono = models.CharField(max_length=8, validators=[MaxLengthValidator(limit_value=8,message='El teléfono debe ser de 8 caracteres')])
     descripcion = models.CharField(max_length=200)
     
--- login/views.py
+++ login/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 from login.models import Formulario
 from django.contrib import messages
 
+
+
+
 # Create your views here.
 #Login
 def iniciar(request):
@@ -30,6 +33,7 @@ def registro(request):
         if request.POST["password1"]!=request.POST["password2"]:
             return render(request, "registro.html", {'form' : UserCreationForm, 'error': "Las contraseñas no coinciden"})
         else:
+            
             name= request.POST["username"]
             password = request.POST["password1"]
             user = User.objects.create_user(username=name,password=password)
@@ -51,17 +55,39 @@ def listado(request):
         'registros': registros
     })
 
+
+
 def guardar(request):
     cliente= request.POST["cliente"]
     rut= request.POST["rut"]
     direccion= request.POST["direccion"]
     fono= request.POST["fono"]
     descripcion= request.POST["descripcion"]
+    
+    if len(fono) !=8:
+        messages.error(request,'El teléfono debe tener 8 dígitos')
+        return redirect('home')
 
-    r = Formulario(cliente=cliente,rut=rut,direccion=direccion,fono=fono,descripcion=descripcion)
-    r.save()
-    messages.success(request, 'Producto agregado')
-    return redirect('listado')
+    def validar_rut(rut):
+
+        rut=rut.replace(".","").replace("-","").lower()
+        if not rut[:-1].isdigit() or not rut[-1] in ('0-9kK'):
+            return False
+        reversed_digits= map(int,reversed(rut[:-1]))
+        factors=[2,3,4,5,6,7,2,3,4,5,6,7]
+        s=sum(d*f for d, f in zip(reversed_digits,factors))
+        expected_digit=(11-s % 11)% 11
+        expected_digit='k' if expected_digit == 10 else str(expected_digit)
+        return expected_digit== rut[-1].lower()
+    
+    if not validar_rut(rut):
+            messages.error(request,'El rut es inválido')
+            return redirect('home')
+    else:
+        r = Formulario(cliente=cliente,rut=rut,direccion=direccion,fono=fono,descripcion=descripcion)
+        r.save()
+        messages.success(request, 'Producto agregado')
+        return redirect('listado')
 
 def detalle(request,id):
     registro= Formulario.objects.get(pk=id)
